8aa138703bf5c929e1b1bd31b8a7f6ea1556ceae
mnkillebr <killebrew.biz@gmail.com>
2019-03-20 11:34:20 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,4 @@
-class Ride < ActiveRecord::Base
+class Ride < ApplicationRecord
+  belongs_to :taxi
+  belongs_to :passenger
 end
--- db/migrate/20140212205242_create_rides.rb
+++
@@ -1,7 +0,0 @@
-class CreateRides < ActiveRecord::Migration
-  def change
-    create_table :rides do |t|
-      t.timestamps null: false
-    end
-  end
-end
